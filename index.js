7af3d91016f7a1448a032fad057d8a259ccbffb5
pashag123 <pashag123@gmail.com>
2023-10-24 14:23:38 -0700

    progression 1

--- index.js
+++ index.js
@@ -7,23 +7,35 @@ const fs = require('fs')
 const questions = [
     {
         type: 'input',
-            name: 'text',
+        name: 'text',
         message: 'enter three characters you want to include in your logo, you can think of these as a three letter abbreviation'
     },
     {
         type: 'input',
-            name: 'textColor',
+        name: 'textColor',
         message: 'select a color you want your Three letter abbreviation to be:'
-            },
+    },
     {
         type: 'list',
-            name: 'shape',
+        name: 'shape',
         message: 'select the shape you want your logo to be:',
         choices: ['circle', 'triangle', 'square']
-                    },
+    },
     {
-        type:
-            name:
-        message:
-                            },
+        type: 'input',
+        name: 'logoColor',
+        message: 'select a color you want your logo to be'
+    },
 ]
+
+const application = () => {
+    inquirer.prompt(questions).then(answers => {
+        try {
+            fs.writeFileSync('examples/logo.svg', shapes(answers))
+            console.log('logo.svg')
+        } catch (error) {
+            console.log(error.message)
+        }
+    })
+}
+application();
