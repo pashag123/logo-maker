5443752bcb9f2d6867d51c4317d3fa893dde423b
pashag123 <pashag123@gmail.com>
2023-10-26 12:35:13 -0700

    almost finished

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer')
-const shapes = require('./lib/shapes')
+const { Circle, Square, Triangle } = require('./lib/shapes')
 const fs = require('fs')
-
+let SVG = require('./lib/svg')
 
 
 const questions = [
@@ -17,22 +17,37 @@ const questions = [
     },
     {
         type: 'list',
-        name: 'shape',
+        name: 'shapeChoice',
         message: 'select the shape you want your logo to be:',
         choices: ['circle', 'triangle', 'square']
     },
     {
         type: 'input',
-        name: 'logoColor',
+        name: 'shapeColor',
         message: 'select a color you want your logo to be'
     },
 ]
 
 const application = () => {
     inquirer.prompt(questions).then(answers => {
-      
-            fs.writeFileSync('examples/logo.svg', shapes(answers))
-            if (err) throw err
+      let shape;
+      if (answers.shapeChoice === 'square') {
+        shape = new Square();
+      } else if (answers.shapeChoice === 'triangle') {
+        shape = new Triangle();
+      }
+      else {
+        shape = new Circle();
+      }
+      shape.setColor(answers.shapeColor);
+      const svg = new SVG;
+      svg.setText(answers.textColor, answers.text );
+      svg.setShape(shape)
+      const logo = svg.render()
+            fs.writeFileSync('examples/logo.svg', logo, (err) => {
+                if (err) throw err
+            
+            })
             console.log('file saved')
         }) 
            
